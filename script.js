0a7430289365c10af2818b3ed9a9bd7484f36d0f
Mudasirh120 <mudasirh120@gmail.com>
2025-04-20 20:45:08 +0500

    UI changed a bit , added all the values required for password generation

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
+const defaultText = `"Protect data with strong password."`;
 const slider = document.querySelector("#range");
 const value = document.querySelector("#value");
-function syncValue() {
+const copy = document.querySelector(".copy");
+const generated = document.querySelector(".password");
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "~!@#$%^&*()_+[]{}|;:,.<>?",
+};
+const password = {
+  length: 8,
+  lowercase: true,
+  uppercase: false,
+  numbers: false,
+  symbols: false,
+};
+const options = ["lowercase", "uppercase", "numbers", "symbols"];
+slider.addEventListener("change", () => {
   value.value = slider.value;
-}
-function syncSlider() {
+});
+value.addEventListener("change", () => {
   slider.value = value.value;
+});
+copy.addEventListener("click", () => {
+  if (generated.textContent.toString() === defaultText) {
+    fetchCharacter(characters[fetchOptions(options)]);
+    // alert("Please generate a password first!");
+  } else {
+    const text = generated.textContent;
+    console.log(text);
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert("Password copied to clipboard!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+function checkPara(paras) {
+  if (paras.lowercase || paras.uppercase || paras.numbers || paras.symbols) {
+    return true;
+  } else {
+    return false;
+  }
 }
-slider.addEventListener("change", syncValue);
-value.addEventListener("change", syncSlider);
+let fetchOptions = function (opt) {
+  return opt[Math.floor(Math.random() * opt.length)];
+};
+function fetchCharacter(str) {
+  console.log(str.charAt(Math.floor(Math.random() * str.length)));
+}
