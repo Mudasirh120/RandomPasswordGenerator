036221829d79008f9abad5f0987eff0c843d09f3
Mudasirh120 <mudasirh120@gmail.com>
2025-04-21 10:22:45 +0500

    logic completed

--- script.js
+++ script.js
@@ -2,31 +2,79 @@ const defaultText = `"Protect data with strong password."`;
 const slider = document.querySelector("#range");
 const value = document.querySelector("#value");
 const copy = document.querySelector(".copy");
+const generate = document.querySelector(".generate");
 const generated = document.querySelector(".password");
+const lower = document.querySelector(".low");
+const upper = document.querySelector(".upp");
+const number = document.querySelector(".num");
+const symbol = document.querySelector(".sym");
+let generatedPassword = "";
 const characters = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers: "0123456789",
   symbols: "~!@#$%^&*()_+[]{}|;:,.<>?",
 };
-const password = {
-  length: 8,
-  lowercase: true,
-  uppercase: false,
-  numbers: false,
-  symbols: false,
-};
-const options = ["lowercase", "uppercase", "numbers", "symbols"];
+const password = {};
+let options = [];
 slider.addEventListener("change", () => {
   value.value = slider.value;
 });
 value.addEventListener("change", () => {
   slider.value = value.value;
 });
+function fetchCheck() {
+  options = [];
+  password.lowercase = lower.checked;
+  password.uppercase = upper.checked;
+  password.numbers = number.checked;
+  password.symbols = symbol.checked;
+  if (password.lowercase) {
+    options.push("lowercase");
+  }
+  if (password.uppercase) {
+    options.push("uppercase");
+  }
+  if (password.numbers) {
+    options.push("numbers");
+  }
+  if (password.symbols) {
+    options.push("symbols");
+  }
+  password.length = slider.value;
+}
+function checkPara(paras) {
+  if (paras.lowercase || paras.uppercase || paras.numbers || paras.symbols) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function fetchOptions(opt) {
+  return opt[Math.floor(Math.random() * opt.length)];
+}
+function fetchCharacter(str) {
+  return str.charAt(Math.floor(Math.random() * str.length));
+}
+function generatePassword() {
+  generatedPassword = "";
+  fetchCheck();
+  if (checkPara(password)) {
+    for (let i = 0; i < password.length; i++) {
+      const randomOption = fetchOptions(options);
+      const randomCharacter = fetchCharacter(characters[randomOption]);
+      generatedPassword += randomCharacter;
+    }
+    generated.textContent = generatedPassword;
+    generated.style.color = "#0075ff";
+  } else {
+    generated.textContent = defaultText;
+  }
+}
 copy.addEventListener("click", () => {
   if (generated.textContent.toString() === defaultText) {
-    fetchCharacter(characters[fetchOptions(options)]);
-    // alert("Please generate a password first!");
+    fetchCheck();
+    alert("Please generate a password first!");
   } else {
     const text = generated.textContent;
     console.log(text);
@@ -36,20 +84,14 @@ copy.addEventListener("click", () => {
         alert("Password copied to clipboard!");
       })
       .catch((err) => {
+        alert("Something Went Wrong!");
         console.log(err);
       });
   }
 });
-function checkPara(paras) {
-  if (paras.lowercase || paras.uppercase || paras.numbers || paras.symbols) {
-    return true;
-  } else {
-    return false;
-  }
-}
-let fetchOptions = function (opt) {
-  return opt[Math.floor(Math.random() * opt.length)];
-};
-function fetchCharacter(str) {
-  console.log(str.charAt(Math.floor(Math.random() * str.length)));
-}
+generate.addEventListener("click", () => {
+  generatePassword();
+});
+copy.addEventListener("mouseover", () => {
+  copy.style.cursor = "pointer";
+});
